727220880ed74f46f134ae292e4af40018bc73a1
ArmaanKafaipour <armaankafaipour1@gmail.com>
2022-03-20 18:05:20 -0700

    import and setup web3reactProvider

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./App";
+import { Web3ReactProvider } from "@web3-react/core";
+import { Web3Provider } from "@ethersproject/providers";
+
+function getLibrary(provider) {
+  return new Web3Provider(provider);
+}
 
 ReactDOM.render(
   <React.StrictMode>
